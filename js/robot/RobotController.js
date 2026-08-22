8bda8ea922a621b2ef87a1ce6c493185343f0883
CodingDrago <gunasai243@gmail.com>
2026-08-22 22:41:10 +0530

    Changed the cache tags.

--- js/robot/RobotController.js
+++ js/robot/RobotController.js
@@ -5,10 +5,10 @@
  */
 
 import * as THREE from 'three';
-import { CONFIG } from '../config.js?v=41';
-import { RobotMaterials } from './RobotMaterials.js?v=41';
-import { RobotArm } from './RobotArm.js?v=41';
-import { TargetMapper } from './TargetMapper.js?v=41';
+import { CONFIG } from '../config.js?v=44';
+import { RobotMaterials } from './RobotMaterials.js?v=44';
+import { RobotArm } from './RobotArm.js?v=44';
+import { TargetMapper } from './TargetMapper.js?v=44';
 
 export class RobotController {
     /**
